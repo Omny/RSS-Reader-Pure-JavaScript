d044c187d46e2c8691c033e5a19eda2dd717de86
Artem Petrishchev <artem.omny@gmail.com>
2023-02-07 14:13:02 +0900

    install on-change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.scss';
 import * as bootstrap from 'bootstrap';
 import * as yup from 'yup';
+import onChange from 'on-change';
 
 const reender = (state) => {
   console.log('Hello World!');
