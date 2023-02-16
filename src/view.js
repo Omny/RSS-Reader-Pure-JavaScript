f8c7866b25a3da7e5d8d960d1996904993b93557
Artem Petrishchev <artem.omny@gmail.com>
2023-02-16 23:42:27 +0900

    split renderPosts into renderPosts and renderFeeds

--- src/view.js
+++ src/view.js
@@ -79,6 +79,52 @@ const renderPosts = (elements, _value, initialState, i18nInstance) => {
     liPosts.push(liPost);
   });
   ulPosts.append(...liPosts);
+};
+
+const renderFeeds = (elements, _value, initialState, i18nInstance) => {
+  const { postsContainer } = elements;
+  postsContainer.innerHTML = '';
+  const cardPosts = document.createElement('div');
+  cardPosts.classList.add('card', 'border-0');
+  postsContainer.append(cardPosts);
+
+  const cardPostsBody = document.createElement('div');
+  cardPostsBody.classList.add('card-body');
+  cardPosts.append(cardPostsBody);
+
+  const cardPostsTitle = document.createElement('h2');
+  cardPostsTitle.classList.add('card-title', 'h4');
+  cardPostsTitle.innerText = i18nInstance.t('posts');
+  cardPostsBody.append(cardPostsTitle);
+
+  const ulPosts = document.createElement('ul');
+  ulPosts.classList.add('list-group', 'border-0', 'rounded-0');
+  cardPosts.append(ulPosts);
+
+  const liPosts = [];
+  initialState.posts.forEach((post) => {
+    const liPost = document.createElement('li');
+    liPost.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const aPost = document.createElement('a');
+    const postClasses = (initialState.uiState.clickedIds.has(post.id)) ? ['fw-normal', 'link-secondary'] : ['fw-bold'];
+    aPost.classList.add(...postClasses);
+    aPost.setAttribute('href', post.link);
+    aPost.setAttribute('data-id', post.id);
+    aPost.setAttribute('rel', 'noopener noreferrer');
+    aPost.setAttribute('target', '_blank');
+    aPost.innerText = post.title;
+    liPost.append(aPost);
+    const buttonPost = document.createElement('button');
+    buttonPost.setAttribute('type', 'button');
+    buttonPost.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    buttonPost.setAttribute('data-id', post.id);
+    buttonPost.setAttribute('data-bs-toggle', 'modal');
+    buttonPost.setAttribute('data-bs-target', '#modal');
+    buttonPost.innerText = i18nInstance.t('view');
+    liPost.append(buttonPost);
+    liPosts.push(liPost);
+  });
+  ulPosts.append(...liPosts);
 
   const { feedsContainer } = elements;
   feedsContainer.innerHTML = '';
@@ -134,6 +180,10 @@ const render = (elements, initialState, i18nInstance) => (path, value) => {
 
     case 'posts':
       renderPosts(elements, value, initialState, i18nInstance);
+      break;
+
+    case 'feeds':
+      renderFeeds(elements, value, initialState, i18nInstance);
       break;
 
     case 'uiState.clickedDataId':
